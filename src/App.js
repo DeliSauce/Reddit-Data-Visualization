ab57dadbd0c1503707706504b13f2559f7763202
DeliSauce <nycdelisauce@gmail.com>
2017-07-21 11:52:06 -0700

    Set up username entry form

--- src/App.js
+++ src/App.js
@@ -3,16 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {username: 'delisauce', userComments: {}, userPosts: {}};
+    this.updateUsername = this.updateUsername.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillUpdate() {
+    console.log(this.state.username);
+  }
+
+  handleSubmit(e) {
+
+  }
+
+  updateUsername (e) {
+    let username = e.currentTarget.value;
+    this.setState({username});
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Reddit Grapher!!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <form onSubmit={ this.handleSubmit } >
+          <label>Enter Reddit User Name
+           <input
+             className="username"
+             value={this.state.username}
+             onChange={this.updateUsername}
+             ></input>
+          </label>
+          <input type='submit' value='Get Data'></input>
+        </form>
+
       </div>
     );
   }
